f0bb12121aa20282bd9a3c326e95939d377f985b
zacharytay1994 <s10179991@connect.np.edu.sg>
2019-10-15 21:22:46 +0800

    fixed naming a little

--- DirectXFramework/SourceFiles/WinMain.cpp
+++ DirectXFramework/SourceFiles/WinMain.cpp
@@ -82,10 +82,10 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	const int wndY = 100; // y-coord of upper left corner
 	const int wndWidth = 800; // in pixels
 	const int wndHeight = 600;
-	const int wndBotX = wndX + wndWidth;
-	const int wndRightY = wndY + wndHeight;
+	const int wndRightX = wndX + wndWidth;
+	const int wndBotY = wndY + wndHeight;
 	// Adjust window rect based on client region
-	RECT rect = { wndX,wndY,wndBotX,wndRightY };
+	RECT rect = { wndX,wndY,wndRightX,wndBotY };
 	if (!AdjustWindowRect(&rect, WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX, false)) {
 		return false;
 	};
