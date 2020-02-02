4d94d74199c94098ae662097e12f4baaa1124422
ngeeannrobin <s10179209@connect.np.edu.sg>
2020-02-02 23:54:59 +0800

    HOW TO ROTATE???

--- DirectXFramework/Player.h
+++ DirectXFramework/Player.h
@@ -2,17 +2,19 @@
 #include "Entity.h"
 #include "Cube.h"
 #include "Vec3.h"
+#include "CubeModel.h"
+
 class Player :
 	public Entity
 {
 private:
-	Cube cube_;
+	//Cube cube_;
 	Vecf3 position_ = { 0.0f, 0.0f, 0.0f };
+	CubeModel cubeModel_;
 public:
 	Player(std::shared_ptr<Graphics> gfx, std::shared_ptr<Input> input);
 	void SetPosition(const Vecf3& position);
 	Vecf3 GetPosition();
-	void SetScale(const Vecf3& scale);
 	void Update(const float& dt) override;
 	void Render(const float& dt) override;
 };
