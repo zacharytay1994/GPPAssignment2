b7a2c364bd9346fa84a5cd1a390ce228a1edfed4
= <sim.sheline@gmail.com>
2020-02-14 14:11:41 +0800

    rails! for-rail!

--- DirectXFramework/MapGenerator.cpp
+++ DirectXFramework/MapGenerator.cpp
@@ -70,7 +70,15 @@ void MapGenerator::GenerateMap()
 			// Generate noise value for current block
 			n = pn_->octaveNoise0_1(x / fx_, z / fz_, octaves_);
 
-			if (abs(x - checkpoint.x) <= 1 && abs(z - checkpoint.z) <= 1 || n > .4 && n < .6) {
+			if ((z - checkpoint.z) == 0 && abs(x - checkpoint.x) <= 1) {
+
+				// temp rails
+				std::shared_ptr<Rail> rail = std::make_shared<Rail>("rail", graphics_, input_, rl_);
+				rail->SetScale({ 0.5, 0.03125, 0.5 });
+				rail->SetPosition(Vecf3(x, -0.5f, z));
+				AddResource({ ResourceBlockType::Rail, 0, 1, rail });
+
+			} else if (abs(z - checkpoint.z) <= 1 && abs(x - checkpoint.x) <= 1|| n > .4 && n < .6) {
 
 				// Set resource_data_
 				resource_data_[3*chunk_width_ * z + (total_map_size_ >= 3 ? 48 + (x - total_map_size_ * chunk_width_) : x)] = { ResourceBlockType::Air, 0, 1, nullptr };
@@ -97,6 +105,108 @@ void MapGenerator::GenerateMap()
 
 	// Increment total_map_size_
  	total_map_size_++;
+}
+
+bool MapGenerator::CanAddRail(std::shared_ptr<Rail> r)
+{
+	// Check if rail can be placed on rail pos
+	auto tile = GetResourceTileData()[(int)(round(r->GetPosition().z) * GetMapSize().x + r->GetPosition().x)];
+	if (tile.block_type_ == ResourceBlockType::Air) {
+
+		if (GetLastRail() == nullptr) {
+			rails_.push_back(r);
+			return true;
+		}
+		else if (abs(GetLastRail()->GetPosition().x - r->GetPosition().x) + abs(GetLastRail()->GetPosition().z - r->GetPosition().z) <= 1) {
+
+			if ((int)round(GetLastRail()->GetPosition().z - r->GetPosition().z) == 1) {
+				// Last rail is above 
+				// Place vertical rail
+				r->SetDirection(Rail::Direction::Vertical);
+
+				// Make correction to second last rail (if needed)
+				if (GetLastRail()->GetDirection() == Rail::Direction::Horizontal) {
+					if (GetSecondLastRail() == nullptr) {
+						GetLastRail()->SetDirection(Rail::Direction::Vertical);
+					}
+					else {
+						if ((GetSecondLastRail()->GetPosition().x - GetLastRail()->GetPosition().x) == -1) {
+							GetLastRail()->SetDirection(Rail::Direction::LBottomCurved);
+						}
+						else {
+							GetLastRail()->SetDirection(Rail::Direction::RBottomCurved);
+						}
+					}
+				}
+			}
+			else if ((int)round(GetLastRail()->GetPosition().z - r->GetPosition().z) == -1) {
+				// Last rail is below
+				// Place vertical rail
+				r->SetDirection(Rail::Direction::Vertical);
+
+				// Make correction to second last rail (if needed)
+				if (GetLastRail()->GetDirection() == Rail::Direction::Horizontal) {
+					if (GetSecondLastRail() == nullptr) {
+						GetLastRail()->SetDirection(Rail::Direction::Vertical);
+					}
+					else {
+						if ((GetSecondLastRail()->GetPosition().x - GetLastRail()->GetPosition().x) == -1) {
+							GetLastRail()->SetDirection(Rail::Direction::LTopCurved);
+						}
+						else {
+							GetLastRail()->SetDirection(Rail::Direction::RTopCurved);
+						}
+					}
+				}
+			}
+			else if ((int)round(GetLastRail()->GetPosition().x - r->GetPosition().x) == 1) {
+				// Last rail is on the right
+				// Place horizontal rail
+				r->SetDirection(Rail::Direction::Horizontal);
+
+				// Make correction to second last rail (if needed)
+				if (GetLastRail()->GetDirection() == Rail::Direction::Vertical) {
+					if (GetSecondLastRail() == nullptr) {
+						GetLastRail()->SetDirection(Rail::Direction::Horizontal);
+					}
+					else {
+						if ((GetSecondLastRail()->GetPosition().z - GetLastRail()->GetPosition().z) == -1) {
+							GetLastRail()->SetDirection(Rail::Direction::LBottomCurved);
+						}
+						else {
+							GetLastRail()->SetDirection(Rail::Direction::LTopCurved);
+						}
+					}
+				}
+			}
+			else if ((int)round(GetLastRail()->GetPosition().x - r->GetPosition().x) == -1) {
+				// Last rail is on the left
+				// Place horizontal rail
+				r->SetDirection(Rail::Direction::Horizontal);
+
+				// Make correction to second last rail (if needed)
+				if (GetLastRail()->GetDirection() == Rail::Direction::Vertical) {
+					if (GetSecondLastRail() == nullptr) {
+						GetLastRail()->SetDirection(Rail::Direction::Horizontal);
+					}
+					else {
+						if ((GetSecondLastRail()->GetPosition().z - GetLastRail()->GetPosition().z) == -1) {
+							GetLastRail()->SetDirection(Rail::Direction::RBottomCurved);
+						}
+						else {
+							GetLastRail()->SetDirection(Rail::Direction::RTopCurved);
+						}
+					}
+				}
+			}
+			else { return false; }
+
+ 			rails_.push_back(r);
+			return true;
+
+		}
+	}
+	return false;
 }
 
 MapGenerator::ResourceTileData** MapGenerator::GetTilesAround(ResourceTileData* starting_tile) {
@@ -155,9 +265,13 @@ MapGenerator::ResourceTileData** MapGenerator::GetTilesAround(ResourceTileData* 
 
 void MapGenerator::AddResource(ResourceTileData tile)
 {
+	if (tile.block_type_ == ResourceBlockType::Rail && !CanAddRail(std::dynamic_pointer_cast<Rail>(tile.ent_))) { return; }
+
 	Vecf3 tile_pos = tile.ent_->GetPosition();
 	int index = (int)(tile_pos.z * chunk_width_ * 3 + round(tile_pos.x));
 	resource_data_[index] = tile;
+
+	scene_->AddEntity(tile.ent_);
 }
 
 void MapGenerator::RemoveResource(ResourceTileData* tile)
